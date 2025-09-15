d191a9372b808d064f5517ecfd7285cf474e87bf
Vatsal Shah <113013039+vatsalshah041@users.noreply.github.com>
2025-09-15 15:37:24 +0530

    lombok

--- trial_test/src/main/java/com/example/trial_test/entity/Categories.java
+++ trial_test/src/main/java/com/example/trial_test/entity/Categories.java
@@ -1,7 +1,12 @@
 package com.example.trial_test.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+
+@Data
 public class Categories {
 
     @Id
@@ -9,27 +14,4 @@ public class Categories {
     private String type;
     private String title;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
